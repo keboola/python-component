63e4ee8bd89ec53c67943d8f6a6f24a17bd3f851
David Esner <david@keboola.com>
2021-02-11 14:17:43 +0100

    bump version to update pypi project

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ project_urls = {
 
 setuptools.setup(
     name="keboola.component",  # TESTING NAME
-    version="1.0.1",
+    version="1.0.2",
     author="Keboola KDS Team",
     project_urls=project_urls,
     setup_requires=['pytest-runner', 'flake8'],
