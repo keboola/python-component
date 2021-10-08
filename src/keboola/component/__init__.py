9ab3215a43b547b8159982767b476272eaa6b9fa
David Esner <david@keboola.com>
2021-10-08 11:08:30 +0200

    bump version, style

--- src/keboola/component/__init__.py
+++ src/keboola/component/__init__.py
@@ -1,4 +1,4 @@
-from .base import ComponentBase
-from .exceptions import UserException
+from .base import ComponentBase  # noqa F401
+from .exceptions import UserException  # noqa F401
 from .interface import CommonInterface, Configuration  # noqa F401
 # __all__ = ["base", "interface", "dao"]
