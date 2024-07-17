693c674aa2d19bfbaa77a6560a02c30e00f7d9de
David Esner <david@keboola.com>
2024-07-17 13:07:40 +0200

    Bump setup.py version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ project_urls = {
 
 setuptools.setup(
     name="keboola.component",
-    version="1.4.4",
+    version="1.6.0",
     author="Keboola KDS Team",
     project_urls=project_urls,
     setup_requires=['pytest-runner', 'flake8'],
