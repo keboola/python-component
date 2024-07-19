b88dcf5014d6a7daf6df415165fdd34011d02598
Josef Kudera <46950237+kudj@users.noreply.github.com>
2024-07-19 10:35:03 +0200

    setup version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ project_urls = {
 
 setuptools.setup(
     name="keboola.component",
-    version="1.6.1",
+    version="1.6.2",
     author="Keboola KDS Team",
     project_urls=project_urls,
     setup_requires=['pytest-runner', 'flake8'],
