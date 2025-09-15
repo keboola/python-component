f370a8e1fece0c07b12c756142df82d3d5de396a
Josef Kudera <46950237+kudj@users.noreply.github.com>
2025-09-15 15:22:08 +0200

    bump setuptools version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ project_urls = {
 
 setuptools.setup(
     name="keboola.component",
-    version="1.6.12",
+    version="1.6.13",
     author="Keboola KDS Team",
     project_urls=project_urls,
     setup_requires=[
